3ebf73426b7e4671b98b2f667eadc65c57e65252
Hézio Silva <103318917+HezioS1lv4@users.noreply.github.com>
2022-10-26 17:14:21 -0300

    Update ex047.py

--- ex047.py
+++ ex047.py
@@ -3,3 +3,11 @@ for n in range(1, 51):  # OU (2, 51, 2):
     if n % 2 == 0:
         print(n, end=' ')
 print('Acabou')
+
+
+# Exemplo para numeros ímpar
+
+for n in range(1,52):
+    if n % 2 == 1:
+        print(n)
+        
