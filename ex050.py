7427a33e5d4af5dcd4573cfaf27df41cbf2904bc
Hézio Silva <103318917+HezioS1lv4@users.noreply.github.com>
2022-10-27 21:25:38 -0300

    Update ex050.py

--- ex050.py
+++ ex050.py
@@ -3,6 +3,6 @@ soma = 0
 for n in range(1, 7):
     num = int(input('Digite o {}° numero inteiro: '.format(n)))
 
-    if num % 2 == 0:
-        soma += num  # OU soma = soma + num
+    if num % 2 == 0: 
+        soma += num  # OU soma = soma + num    # num se par, vira soma que tem valor de 0
 print('A soma dos valores PARES é: {} '.format(soma))
