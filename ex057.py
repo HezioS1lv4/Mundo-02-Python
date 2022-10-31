5622a003e4ed534b1c274e03fe11fcedf1bc6e59
Hézio Silva <103318917+HezioS1lv4@users.noreply.github.com>
2022-10-31 17:08:12 -0300

    Update ex057.py

--- ex057.py
+++ ex057.py
@@ -1,7 +1,7 @@
 sexo = 'Mm, Ff'
 
 while sexo == 'Mm' or 'Ff':
-    sexo = str(input('Qual o seu sexo? [M/F] ')).upper()
+    sexo = str(input('Qual o seu sexo? [M/F] ')).upper()[0].strip()
     if sexo == 'M':
         print('Seu sexo é Masculino!')
     if sexo == 'F':
@@ -9,3 +9,9 @@ while sexo == 'Mm' or 'Ff':
 
     if sexo != 'M' and sexo != 'F':
         print('Escolha uma opção válida!')
+
+# EX do guanabara
+sexo = str(input('Informe seu sexo: [M/F] ')).strip().upper()[0]
+while sexo not in 'MmFf':
+    sexo = str(input('Dados invalidos. Por favor, informe seu sexo: [M/F] ')).strip().upper()[0]
+print('Sexo {} registrado com sucesso'.format(sexo))
