24625d554a3ebecfec500befaf045fd66df70334
Hézio Silva <103318917+HezioS1lv4@users.noreply.github.com>
2022-10-31 18:52:26 -0300

    Add files via upload

---
+++ ex058.py
@@ -0,0 +1,19 @@
+from random import randint
+pc = randint(0, 10)
+
+print('Sou seu computador...')
+print('Acabei de pensar em um número entre 0 e 10.')
+print('Será que você consegue adivinhar qual foi? ')
+acertou = False
+palpites = 0
+while not acertou:
+    jogador = int(input('Qual é o seu palpite? '))
+    palpites += 1
+    if jogador == pc:
+        acertou = True
+    else:
+        if jogador < pc:
+            print('Mais...')
+        elif jogador < pc:
+            print('Menos...')
+print('Acertou com {} palpites!'.format(palpites))
